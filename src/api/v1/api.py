0f7e8f01c172df1bc5225ea528564882c634305a
User joker96824 <715044744@qq.com>
2025-05-07 16:34:05 +0800

    解决api路径重复问题

--- src/api/v1/api.py
+++ src/api/v1/api.py
@@ -4,4 +4,4 @@ from .endpoints import cards
 api_router = APIRouter()
 
 # 注册卡牌相关路由
-api_router.include_router(cards.router, prefix="/cards", tags=["cards"]) 
+api_router.include_router(cards.router, tags=["cards"])
